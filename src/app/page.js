dcc66f3ac9d9c1166861d22aceddd471e7f10c8c
veasnabig <veasnakaobig@gmail.com>
2023-12-31 01:03:22 +0700

    update textin page

--- src/app/page.js
+++ src/app/page.js
@@ -38,7 +38,7 @@ export default function Home() {
           priority
         />
       </div>
-
+      <h1 className='text-3xl'>NextJs CICD</h1>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
